e7f9976c7f10373247594fecf210d9648cce00fa
larrynet <lawrenceng3@hotmail.com>
2017-02-04 15:26:47 -0500

    Add test program for server

---
+++ client.py
@@ -0,0 +1,20 @@
+# The scope of this application is
+import Image
+import json
+
+#start recording the moment incident happens
+# package json
+
+#extract all coordinates
+#sample driver data:
+driverFName = "Lawrence"
+driverLName = "Ng"
+driverFace  = Image.open("c:\\project\\driver_face.jpg")
+driverIncident = "c:\\project\\driver.avi"
+
+#sample of other driver:
+otherDriverFName = "Dylan"
+otherDriverLName = "Fernandes"
+otherDriverFace = Image.open("project\\other_driver_face.jpg")
+driverOIncident = "project\\otherdriver.avi"
+
