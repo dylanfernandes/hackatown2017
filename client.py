6c44d87fc2c40c59d563de252e61fd6e62afa3b8
larrynet <lawrenceng3@hotmail.com>
2017-02-04 20:20:06 -0500

    Add real driver face and modify client.php for testing

--- client.py
+++ client.py
@@ -1,20 +1,30 @@
-# The scope of this application is
-import Image
 import json
+import httplib
+headers = { "charset" : "utf-8", "Content-Type": "application/json" }
 
-#start recording the moment incident happens
-# package json
+#refer from http://stackoverflow.com/questions/28778384/sending-json-data-from-python-to-php
+#missing avi and image file to send (maybe through ssh)?
 
 #extract all coordinates
-#sample driver data:
-driverFName = "Lawrence"
-driverLName = "Ng"
-driverFace  = Image.open("c:\\project\\driver_face.jpg")
-driverIncident = "c:\\project\\driver.avi"
+driverLicensePlate = '123456'
+driverName = 'Lawrence'
 
 #sample of other driver:
-otherDriverFName = "Dylan"
-otherDriverLName = "Fernandes"
-otherDriverFace = Image.open("project\\other_driver_face.jpg")
-driverOIncident = "project\\otherdriver.avi"
+otherDriverLicensePlate = '567890'
+
+data = {
+	'licensePlate' = driverLicensePlate
+	'driverFName'  = driverName
+
+	
+json_str = json.dumps(data)
+conn = httplib.HTTPConnection("localhost")
+
+conn.request("POST", "/process.php", json_str, headers)
+
+response = conn.getresponse()
+
+print(response.read())
+
+conn.close()
 
