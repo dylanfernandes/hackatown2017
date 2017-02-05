e275e5bb1df36b78fa9bdf22f5d2682ec9a8f001
Dylan Fernandes <dylangfernandes@gmail.com>
2017-02-04 22:58:01 -0500

    AJAX working

---
+++ entry.php
@@ -0,0 +1,41 @@
+<?php
+class Entry
+{
+	var $rName;
+	var $rPlate;
+   var $oPlate;
+
+	function Entry($rName, $rPlate,$oPlate) 
+   {
+       $this->rName = $rName;
+       $this->$rPlate = $rPlate;
+       $this->$oPlate = $oPlate;
+   } 
+
+   function getRName()
+   {
+   	return $this->rName;
+   }
+   function getRPlate()
+   {
+   	return $this->rPlate;
+   }
+   function getOPlate()
+   {
+      return $this->oPlate;
+   }
+
+   function setRName($name)
+   {
+   	$this->rName = $name;
+   }
+  function setRPlate($oPlate)
+   {
+      $this->rPlate= $rPlate;
+   }
+   function setOPlate($oPlate)
+   {
+      $this->oPlate= $oPlate;
+   }
+}
+?>
--- process.php
+++ process.php
@@ -1,5 +1,34 @@
 <?php
-$arr = array("Incident Number"=> "1", "Incident Reporter" => "Mark Tester", "License Plate Reporter" => "A12 BCD", "License Plate Other" => "A12 BCD");
-
-echo json_encode($arr);
+require_once('entry.php');
+$myfile = fopen("data.txt", "r") or die("Unable to open file!");
+$content = fread($myfile,filesize("data.txt"));
+$people = explode("\n", $content);
+$existing = array();
+$string = "<table class=\"table\">
+  <thead class=\"thead-inverse\">
+    <tr>
+      <th>Incident Number</th>
+      <th>Incident Reporter</th>
+      <th>License Plate Reporter</th>
+      <th>License Plate Other</th>
+    </tr>
+  </thead>
+  <tbody>";
+for($i = 0;$i < sizeof($people);$i++)
+{
+	$info = explode(':',$people[$i]);
+	$temp = new Entry($info[0],$info[1],$info[2]);
+	//echo "<br /> The Password is: ".$temp->userName;
+	$existing[$i] =  $temp;
+}
+for($j = 0;$j < sizeof($existing);$j++)
+{
+	//echo "<br /> The Password is: ".$existing[$j]->userName;
+	echo $existing[$j]->getRName();
+	
+}
+fclose($myfile);
+$string = $string. "</tbody>
+</table>";
+echo $string;
 ?>
