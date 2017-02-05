1bb00d623c8a03c4c66fa6e914a2076261472f44
Dylan Fernandes <dylangfernandes@gmail.com>
2017-02-04 20:51:11 -0500

    web UI inital added

---
+++ process.php
@@ -0,0 +1,5 @@
+<?php
+$arr = array("Incident Number"=> "1", "Incident Reporter" => "Mark Tester", "License Plate Reporter" => "A12 BCD", "License Plate Other" => "A12 BCD");
+
+echo json_encode($arr);
+?>
